c76afb7b5b6c6a55b179b384a2b7a12c5e2db855
mechanicalpanda <33541225+mechanicalpanda@users.noreply.github.com>
2025-10-20 21:20:53 +0800

    Update Ctest.c

---
+++ Unity/Ctest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    printf("Hello, GitHub Actions!\n");
+
+    int num = 5;
+    printf("Factorial of %d is %d\n", num, factorial(num));
+
+    return 0;
+}
